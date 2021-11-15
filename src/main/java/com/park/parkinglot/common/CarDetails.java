0f49c113e88cd8088616a94729c989dd45e0b3b6
cristivecerdea <vecerdeacristian@gmail.com>
2021-11-15 11:20:51 +0200

    added EJB

---
+++ src/main/java/com/park/parkinglot/common/CarDetails.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.park.parkinglot.common;
+
+/**
+ *
+ * @author User
+ */
+public class CarDetails implements java.io.Serializable {
+    
+       private Integer id;
+       
+       private String licensePlate;
+       
+       private String parkingSpot;
+       
+       private String username;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public CarDetails(Integer id, String licensePlate, String parkingSpot, String username) {
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.parkingSpot = parkingSpot;
+        this.username = username;
+    }
+       
+       
+}
---
+++ src/main/java/com/park/parkinglot/ejb/CarBean.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package com.park.parkinglot.ejb;
+
+import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.entity.Car;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author User
+ */
+@Stateless
+public class CarBean {
+
+    private static final Logger LOG = Logger.getLogger(CarBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<CarDetails> getAllCars() {
+        LOG.info("getAllCars");
+        try {
+            List<Car> cars = (List<Car>) em.createQuery("SELECT c FROM Car c").getResultList();
+            return copyCarsToDetails(cars);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+    
+    public List<CarDetails> copyCarsToDetails(List<Car> cars) {
+        List<CarDetails> detailsList = new ArrayList<>();
+        for (Car  car : cars) {
+            CarDetails carDetails = new CarDetails(car.getId(),
+                car.getLicensePlate(),
+                car.getParkingSpot(),
+                car.getUser().getUsername());
+            detailsList.add(carDetails);
+        }
+        return detailsList;
+    }
+}
