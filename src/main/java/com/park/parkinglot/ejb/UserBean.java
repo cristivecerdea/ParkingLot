e1a1981f98abfb95c485b69c1ce26092a1a8de90
cristivecerdea <vecerdeacristian@gmail.com>
2022-01-09 18:53:04 +0200

    -created a session scoped bean

--- src/main/java/com/park/parkinglot/ejb/UserBean.java
+++ src/main/java/com/park/parkinglot/ejb/UserBean.java
@@ -7,6 +7,7 @@ package com.park.parkinglot.ejb;
 import com.park.parkinglot.common.UserDetails;
 import com.park.parkinglot.entity.User;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.ejb.EJBException;
@@ -21,22 +22,22 @@ import javax.persistence.Query;
  */
 @Stateless
 public class UserBean {
-    
+
     private static final Logger LOG = Logger.getLogger(UserBean.class.getName());
-    
+
     @PersistenceContext
     private EntityManager em;
-    
+
     public void createUser(String username, String email, String passwordSha256, String position) {
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(passwordSha256);
         user.setPosition(position);
-        
+
         em.persist(user);
     }
-    
+
     public List<UserDetails> getAllUsers() {
         LOG.info("getAllUsers");
         try {
@@ -47,9 +48,9 @@ public class UserBean {
             throw new EJBException(ex);
         }
     }
-    
+
     private List<UserDetails> copyUsersToDetails(List<User> users) {
-        
+
         List<UserDetails> detailsList = new ArrayList<>();
         for (User user : users) {
             UserDetails userDetails = new UserDetails(user.getId(),
@@ -59,5 +60,11 @@ public class UserBean {
             detailsList.add(userDetails);
         }
         return detailsList;
+    }
+
+    public Collection<String> findUsernames(Collection<Integer> userIds) {
+        LOG.info("findUsernames");
+        List<String> usernames = (List<String>) em.createQuery("SELECT u.username FROM User u WHERE u.id IN ?1").setParameter(1, userIds).getResultList();
+        return usernames;
     }
 }
--- src/main/java/com/park/parkinglot/servlet/user/Users.java
+++ src/main/java/com/park/parkinglot/servlet/user/Users.java
@@ -5,9 +5,13 @@
 package com.park.parkinglot.servlet.user;
 
 import com.park.parkinglot.common.UserDetails;
+import com.park.parkinglot.ejb.InvoiceBean;
 import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
@@ -28,14 +32,21 @@ public class Users extends HttpServlet {
     @Inject
     private UserBean userBean;
 
+    @Inject
+    private InvoiceBean invoiceBean;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setAttribute("activePage", "Users");
-        request.setAttribute("numberOfFreeParkingSpots", 10);
 
         List<UserDetails> users = userBean.getAllUsers();
         request.setAttribute("users", users);
+
+        if (!invoiceBean.getUserIds().isEmpty()) {
+            Collection<String> usernames = userBean.findUsernames(invoiceBean.getUserIds());
+            request.setAttribute("invoices", usernames);
+        }
 
         request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
     }
@@ -43,7 +54,16 @@ public class Users extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String[] userIdsAsString = request.getParameterValues("user_ids");
+        if (userIdsAsString != null) {
+            Set<Integer> userIds = new HashSet<Integer>();
+            for (String userIdAsString : userIdsAsString) {
+                userIds.add(Integer.parseInt(userIdAsString));
+            }
 
+            invoiceBean.getUserIds().addAll(userIds);
+        }
+        response.sendRedirect(request.getContextPath() + "/Users");
     }
 
     @Override
